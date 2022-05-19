86a1c15d8d9d09dfe4014839a963cfabd58a35b7
onurfeyyaz <feyyazonur0@gmail.com>
2022-05-19 17:56:54 +0300

    add textfields and test send project to GitHub

--- movielist/ViewController.swift
+++ movielist/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name_tfield: UITextField!
+    @IBOutlet weak var email_tfield: UITextField!
+    @IBOutlet weak var password_tfield: UITextField!
+    
+    @IBOutlet weak var repassword_tfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
